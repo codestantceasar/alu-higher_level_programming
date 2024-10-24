315027769100b79f4fb919cfbc492118b4279656
codestantceasar <c.akas@alustudent.com>
2024-10-24 15:16:58 -0700

    Task 1: Add 1-element_at.py to retrieve list elements safely

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """
+    Retrieves an element from a list at a specified index.
+
+    Args:
+        my_list (list): The list from which to retrieve the element.
+        idx (int): The index of the element to retrieve.
+
+    Returns:
+        int or None: The element at the specified index, or None if index is invalid.
+    """
+    if not isinstance(idx, int):
+        return None
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+    return my_list[idx]
