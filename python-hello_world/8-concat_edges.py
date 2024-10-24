22a49fcf089d8f049e466a44f7d8234221e97cef
codestantceasar <c.akas@alustudent.com>
2024-10-24 08:36:43 -0700

    Task 8: Add 8-concat_edges.py to print object-oriented programming with Python without string literals

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+print(
+    chr(111) + chr(98) + chr(106) + chr(101) + chr(99) + chr(116) + chr(45) +
+    chr(111) + chr(114) + chr(105) + chr(101) + chr(110) + chr(116) + 
+    chr(101) + chr(100) + chr(32) + chr(112) + chr(114) + chr(111) + 
+    chr(103) + chr(114) + chr(97) + chr(109) + chr(109) + chr(105) + 
+    chr(110) + chr(103) + chr(32) + chr(119) + chr(105) + chr(116) + 
+    chr(104) + chr(32) + chr(80) + chr(121) + chr(116) + chr(104) + 
+    chr(111) + chr(110)
+)
