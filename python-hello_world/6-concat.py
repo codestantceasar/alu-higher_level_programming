90413fdb6c89316951512fdef0911534e19a64ca
codestantceasar <c.akas@alustudent.com>
2024-10-24 08:10:39 -0700

    Task 6: Add 6-concat.py to concatenate two strings with a space

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Welcome to "
-str2 = "Battery Street!"
-print(str1 + str2)
+str1 = "Welcome to Holberton School!"
+str2 = str1.replace("Holberton School", "Battery Street")
+print(str2)
