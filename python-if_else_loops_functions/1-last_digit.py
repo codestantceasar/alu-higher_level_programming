6710183491fbf1100132ae2a8ab31fe6bbac6981
codestantceasar <c.akas@alustudent.com>
2024-10-24 11:09:10 -0700

    Task 1: Add 1-last_digit.py to determine the last digit of a random number

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+# Compute the last digit
+if number >= 0:
+    last_digit = number % 10
+else:
+    last_digit = -(-number % 10)
+
+# Print the first part of the message
+print(f"Last digit of {number} is {last_digit} and is ", end="")
+
+# Determine and print the appropriate message
+if last_digit > 5:
+    print("greater than 5")
+elif last_digit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
