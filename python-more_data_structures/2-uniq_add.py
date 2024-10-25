ae6ab2f518536234da8eadf6e94258582efdd8ba
codestantceasar <c.akas@alustudent.com>
2024-10-24 19:47:29 -0700

    Add solution for task 2: unique addition

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    # Convert the list to a set to get unique values, then sum them
+    unique_values = set(my_list)
+    total_sum = sum(unique_values)
+    return total_sum
