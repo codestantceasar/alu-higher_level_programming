eeca7a70a86562cbc389abeaf1412ecb92e83e97
codestantceasar <c.akas@alustudent.com>
2024-10-24 17:08:57 -0700

    Task 8: Add function to return tuple with length and first character of string.

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    if sentence == "":
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
+
+
+# Test cases from the provided main.py file
+if __name__ == "__main__":
+    multiple_returns = __import__('8-multiple_returns').multiple_returns
+
+    sentence = "At school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
