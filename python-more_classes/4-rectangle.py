f63efc0ef9aba6bae2a9cbbcfc4fc31cd1bede8e
codestantceasar <c.akas@alustudent.com>
2024-11-02 03:29:09 -0700

    Fix PEP8 E501 error by adjusting line length in __repr__ method

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -111,6 +111,5 @@ class Rectangle:
         Returns:
             str: A string that can be used with eval() to create a new instance.
         """
-        return "Rectangle({}, {})".format(
-            self.__width, self.__height
-        )
+        return ("Rectangle({}, {})"
+                .format(self.__width, self.__height))
