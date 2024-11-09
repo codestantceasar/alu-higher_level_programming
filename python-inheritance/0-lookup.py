55b7def4844620da74ef0d0b966e6efd57a01649
codestantceasar <c.akas@alustudent.com>
2024-11-09 08:53:51 -0800

    Add files to look up list

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Module to provide a method to list available attributes and methods of an object."""
+"""Module that provides a function to list attributes and methods of an object.
+"""
+
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object"""
+    """Return the list of available attributes and methods of an object."""
     return dir(obj)
