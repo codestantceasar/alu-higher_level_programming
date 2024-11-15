8c2b1973203efa1b95ca3a6ddb60698ef822344b
codestantceasar <c.akas@alustudent.com>
2024-11-15 06:58:37 -0800

    Add this files

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -5,7 +5,7 @@
 This module provides a function to convert a Python object to its JSON string
 
 Functions:
-    to_json_string(my_obj): Returns the JSON representation of a Python object as a string.
+    to_json_string(my_obj): Returns the JSON representation of a Python object.
 """
 
 import json
