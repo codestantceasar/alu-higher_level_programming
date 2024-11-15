b1b5cb70aaf5151207ecfb502bc60080c9cef3e2
codestantceasar <c.akas@alustudent.com>
2024-11-15 11:35:12 -0800

    Add this files

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -49,7 +49,7 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]  # Start with 1
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])  # Sum of two numbers from the previous row
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)  # End with 1
         triangle.append(row)
 
