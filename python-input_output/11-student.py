30020ad04c60cce64a897f294d794e96e572ceb2
codestantceasar <c.akas@alustudent.com>
2024-11-15 11:06:34 -0800

    Add this file

---
+++ python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Student class definition with serialization and deserialization """
+
+
+class Student:
+    """ Defines a student with first name, last name, and age """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializes the student with given attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of the Student instance
+            If attrs is provided, only attributes in that list are included.
+        """
+        if attrs is None:
+            return self.__dict__
+
+        # Filter attributes based on the given list of attributes
+        filtered_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                filtered_dict[attr] = getattr(self, attr)
+        return filtered_dict
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance with the given dictionary """
+        for key, value in json.items():
+            setattr(self, key, value)
