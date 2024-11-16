eed4bcd734146ff071f2cd6ee94a72a54145eda1
codestantceasar <c.akas@alustudent.com>
2024-11-16 09:29:09 -0800

    Add this file

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,16 @@
-USE hbtn_0d_2;
 -- A script that creates the table force_name on your MySQL server
 -- Query to create a table 
 CREATE TABLE IF NOT EXISTS force_name (
        id INT,
        name VARCHAR(256) NOT NULL);
+
+-- Insert data into the table
+INSERT INTO `force_name` (`name`) VALUES
+('Holberton School'),
+('Python is cool'),
+('Holberton'),
+('School'),
+('C is fun');
+
+-- Query the table
+SELECT `id`, `name` FROM `force_name`;
