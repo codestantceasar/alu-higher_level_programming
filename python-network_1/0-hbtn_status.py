78d242e99cbaf2ec34e60b6609e7cfd5e6a06e27
codestantcaesar <c.akas@alustudent.com>
2024-11-26 15:33:57 +0000

    Add this files

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""Fetches https://alu-intranet.hbtn.io/status and displays response info."""
 import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
         body = response.read()
         print("Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body}")
-        print(f"\t- utf8 content: {body.decode('utf-8')}")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
