05e5f30ce10143337bc49319ce7806cb0b7cd6af
codestantcaesar <c.akas@alustudent.com>
2024-11-26 15:40:22 +0000

    Add this files

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status')respons:
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
