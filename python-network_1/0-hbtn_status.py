2cefcf0006e4a09ff5b23589791856f9cfeabc19
codestantcaesar <c.akas@alustudent.com>
2024-11-26 15:04:48 +0000

    Add this files

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 """
-# A Python script that fetches http://0.0.0.0:5050/status using urllib
-
+A Python script that fetches https://alu-intranet.hbtn.io/status using urllib
+"""
 import urllib.request
 
-# URL to be fetched
 url = "http://0.0.0.0:5050/status"
 
-# Using urllib to send a request to the URL and fetch its response
 with urllib.request.urlopen(url) as response:
-    # Read the response body
     body = response.read()
-    
-    # Print details about the response body
     print("Body response:")
-    print("\t- type: {}".format(type(body)))  # Print the type of the response content
-    print("\t- content: {}".format(body))      # Print the raw content of the response
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))  # Print the response content decoded as UTF-8
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
