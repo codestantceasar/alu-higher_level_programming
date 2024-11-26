afa7bd47b82133a9326ee0c7932cf7f64fcded00
codestantcaesar <c.akas@alustudent.com>
2024-11-26 16:17:45 +0000

    Add this files

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,42 +1,17 @@
 #!/usr/bin/python3
 """
-Sends a GET request to a URL and displays the body of the response.
-
-If the HTTP status code is >= 400, prints "Error code: <status_code>".
-
-Usage:
-    ./3-error_code.py <URL>
+Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
+(handling HTTP errors)
 """
-
-import urllib.request
-import urllib.error
+from urllib import request, error
 import sys
 
 
-def main():
-    """
-    Handle the HTTP GET request and process the response.
-
-    Args:
-        None
-
-    Returns:
-        None
-
-    Prints:
-        - The body of the response if the status code is 200.
-        - "Error code: <status_code>" if the status code is >= 400.
-    """
-    url = sys.argv[1]
-    req = urllib.request.Request(url)
-
-    try:
-        with urllib.request.urlopen(req) as response:
-            body = response.read().decode('utf-8')
-            print(body)
-    except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
-
-
 if __name__ == "__main__":
-    main()
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code: {}'.format(err.code))
