02ad8830ea1b6b641447e1d126f9a2ad29ebf13a
codestantcaesar <c.akas@alustudent.com>
2024-11-26 14:53:12 +0000

    Add this files

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A Python script that fetches https://alu-intranet.hbtn.io/status using urllib
+"""
+import urllib.request
+
+url = "https://alu-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
