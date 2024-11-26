a2707715d1017e7776416cf5c4aea7036f26d43d
codestantcaesar <c.akas@alustudent.com>
2024-11-26 08:37:44 +0000

    Add script that displays the size of the body of the response

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s "$1" | wc -c
+# Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+curl -s -o /dev/null -w '%{size_download}\n' "$1"
