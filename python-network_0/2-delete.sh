6ff1e888ef4080ebf35a66590e4ce7c28267d99e
codestantcaesar <c.akas@alustudent.com>
2024-11-26 14:42:55 +0000

    Add this new file

--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
+# Script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -sL -X DELETE "$1"
