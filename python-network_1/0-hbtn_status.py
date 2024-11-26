7d6e076d6362c7ea9cb62b4213e1839400203a96
codestantcaesar <c.akas@alustudent.com>
2024-11-26 14:57:33 +0000

    Add this files

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-A Python script that fetches https://alu-intranet.hbtn.io/status using urllib
+A Python script that fetches http://0.0.0.0:5050/status using urllib
 """
 import urllib.request
 
-url = "https://alu-intranet.hbtn.io/status"
+url = "http://0.0.0.0:5050/status"
 
 with urllib.request.urlopen(url) as response:
     body = response.read()
