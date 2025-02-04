439f436b63d0f12eb15d6b10158d75b09fee4bac
codestantcaesar <c.akas@alustudent.com>
2025-02-04 15:12:39 -0800

    Add this files

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    const films = data.results;
+    
+    const count = films.filter(film =>
+      film.characters.some(character => character.includes(`/people/${characterId}/`))
+    ).length;
+
+    console.log(count);
+  }
+});
---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+6. How many completed?
+mandatory
+Write a script that computes the number of tasks completed by user id.
+
+The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
+Only print users with completed task
+You must use the module request
+guillaume@ubuntu:~/$ ./6-completed_tasks.js https://jsonplaceholder.typicode.com/todos
+{ '1': 11,
+  '2': 8,
+  '3': 7,
+  '4': 6,
+  '5': 12,
+  '6': 6,
+  '7': 9,
+  '8': 11,
+  '9': 8,
+  '10': 12 }
+guillaume@ubuntu:~/$
+Repo:
+
+GitHub repository: alu-higher_level_programming
+Directory: javascript-web_scraping
+File: 6-completed_tasks.js
